8b0ef57db26f9794bb3d9ff0421ad1b60f22daa8
Tai Vuong <tai.vuong@s3corp.com.vn>
2019-12-31 16:38:20 +0700

    making the api better

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -9,10 +9,66 @@ exports.checkBody = (req, res, next) => {
     next();
 }
 
+exports.aliasTopTours = (req, res, next) => {
+    req.query.limit='5';
+    req.query.sort='-ratingsAverage,price';
+    req.query.fields='name,price,ratingsAverage,summary,difficulty';
+    next();
+}
+
 exports.getAllTour = async (req, res) => {
     try {
-        const tours = await  Tour.find()
+        // BUILD QUERY
+        // 1)Filtering
+        const queryObj = {...req.query};
+        const excludeFiled = ['page', 'sort', 'limit', 'fields'];
+        excludeFiled.forEach(el => delete queryObj[el]);
 
+        
+        // 1B)ADVANCED FILTERING
+        let queryStr = JSON.stringify(queryObj);
+        queryStr =  queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+        console.log(JSON.parse(queryStr));
+        
+        
+        let query = Tour.find(JSON.parse(queryStr));
+
+
+        // 2) SORTING
+        if(req.query.sort){
+            const sortBy = req.query.sort.split(',').join(' ');
+            query = query.sort(sortBy);
+        }else{
+            query =query.sort('-createdAt');
+        }
+
+        // 3 Field limiting
+        if(req.query.fields){
+            const fields = req.query.fields.split(',').join(' ');
+            query = query.select(fields);
+        }else{
+            query = query.select('-__v');
+        }
+
+        // 4 Pagination
+        const page = req.query.page * 1 || 1;
+        const limit = req.query.limit * 1 || 100;
+        const skip = (page - 1) * limit;
+        //page=2&limit=10 1-10, page 1 , 11-20, page 2, 21-30, page 3
+        query = query.skip(skip).limit(limit);
+
+        if(req.query.page){
+            const numTours = await Tour.countDocuments();
+            if(skip >= numTours) throw new Error('This page does not exist')
+        }
+
+        //EXECUTE QUERY
+        const tours = await  query;
+        // query.sort().select().skip().limit();
+        // const tours = await Tour.find(req.query);
+        // const tours = await Tour.find().where('duration').equals(5).where('difficulty').equals('easy');
+
+        // SEND QUERY
         res.status(200).json({
             status:'success',
             requestedAt: req.requestTime,
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express')
-const {getAllTour ,createTour, getTour, updateTour, deleteTour,checkID, checkBody} = require('./../controllers/tourController');
+const {aliasTopTours,getAllTour ,createTour, getTour, updateTour, deleteTour,checkID, checkBody} = require('./../controllers/tourController');
 
 const router = express.Router();
 
 // router.param('id', checkID)
+
+router.route('/top-5-cheap').get(aliasTopTours,getAllTour)
 
 router.route('/').get(getAllTour).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
