c21f57fcd2fa626e4f548b58906fb98a7e6887ed
TaiVuong <taivt95@gmail.com>
2020-01-03 07:47:06 +0700

    modelling user and managing password

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,11 @@
 
 const express = require('express')
 const {getAllUser ,createUser, getUser, updateUser, deleteUser} = require('./../controllers/userController');
+const {signup} = require('./../controllers/authController');
 
 const router = express.Router();
+
+router.post('/signup', signup);
 
 
 router.route('/').get(getAllUser).post(createUser);
