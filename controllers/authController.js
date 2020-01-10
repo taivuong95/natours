8ffdad4190670ec4149e457714eebfe206f7ac08
Tai Vuong <tai.vuong@s3corp.com.vn>
2020-01-10 14:39:14 +0700

    add signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,28 @@
-const User =require('./../models/userModel')
+const User =require('./../models/userModel');
+const jwt = require('jsonwebtoken')
 
 exports.signup = async (req, res, next) => {
     const newUser = await User.create(req.body);
 
+    const token = jwt.sign({ id: newUser._id}, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+    })
+
     res.status(201).json({
         status:'success',
+        token:token,
         data:{
             user: newUser
         }
     })
+}
+
+exports.login = (req, res, next) => {
+    const {email, password} = req.body;
+    
+    // 1) Check if email and password exist
+
+    // 2) Check if user exists && password is correct
+
+    // 3) If everything is ok, send token to client
 }
