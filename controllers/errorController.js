c99458ccd6f2374f39f021758735f52032db4d7a
Tai Vuong <tai.vuong@s3corp.com.vn>
2020-01-30 09:34:01 +0700

    update handle error

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,7 +1,23 @@
 const AppError = require('./../utils/appError')
 
-handleCastErrorDB = err => {
+const handleCastErrorDB = err => {
     const message = `Invalid ${err.path}: ${err.value}.`;
+    return new AppError(message, 404);
+}
+
+const handleDuplicateFieldsDB = err => {
+    const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+    console.log(value);
+    
+    const message = `Duplicate field value: x. Please use another value!`
+    return new AppError(message, 404);
+}
+
+const handleValidationErrorDB = err => {
+
+    const errors = Object.values(err.errors).map(el => el.message);
+
+    const message = `Invalid input data. ${errors.join('. ')}`;
     return new AppError(message, 404);
 }
 
@@ -53,6 +69,10 @@ module.exports = (err, req, res, next) => {
 
         if(error.name = 'CastError') error = handleCastErrorDB(error)
 
+        if(error.code === 11000) error = handleDuplicateFieldsDB(error);
+
+        if(error.name === 'ValidationError')
+            error = handleValidationErrorDB(error)
 
         sendErrorProd(error, res);
     }
--- server.js
+++ server.js
@@ -21,6 +21,17 @@ mongoose
 
 // 4) START SERVER
 const port = process.env.PORT || 8000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`App running on port ${port}`)
+})
+
+
+process.on("unhandledRejection", err => {
+    console.log(err.name, err.message);
+    console.log('unhandledRejection Shutdown');
+    server.close(() => {
+
+        process.exit(1);
+    })
+    
 })
