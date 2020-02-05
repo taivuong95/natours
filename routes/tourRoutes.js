1b6fa55ef61ece6c683e0d633ca84e80de879bb2
Tai Vuong <tai.vuong@s3corp.com.vn>
2020-02-05 10:07:14 +0700

    User role and permision

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -12,7 +12,9 @@ router.route('/tour-stats').get(tourController.getTourStats);
 router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
 router.route('/').get(authController.protect, tourController.getAllTour).post(tourController.createTour);
-router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
+router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(authController.protect,
+     authController.restrictTo('admin', 'lead-guide'),
+      tourController.deleteTour);
 
 
 module.exports = router;
