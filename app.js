4732374b9be91e987fdb7e11524fb9b843517a82
Tai Vuong <taivt1195@gmail.com>
2020-11-13 10:15:06 +0700

    Preventing Parameter Pollution

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const morgan = require('morgan');
 const rateLimit = require('express-rate-limit')
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
-const xss = require('xss-clean')
+const xss = require('xss-clean');
+const hpp = require('hpp')
 
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
@@ -39,6 +40,11 @@ app.use(mongoSanitize());
 
 // Data sanitization against XSS
 app.use(xss());
+
+// Prevent parameter pollution
+app.use(hpp({
+    whitelist:['duration', 'ratingsQuantity', 'ratingsAverage', 'maxGroupSize', 'difficulty', 'price']
+}));
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`))
