74dea33cb5997207baad4d5e282cab72594fac7a
Tai Vuong <taivt1195@gmail.com>
2020-11-16 09:46:26 +0700

    Factory Fuctions: Update and Create

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -12,8 +12,9 @@ const router = express.Router({
     .route('/')
     .get(reviewController.getAllReviews)
     .post(authController.protect, authController.restrictTo('user'),
+    reviewController.setTourUserIds,
     reviewController.createReview);
 
-    router.route('/:id').delete(reviewController.deleteReview);
+    router.route('/:id').patch(reviewController.updateReview).delete(reviewController.deleteReview);
 
 module.exports = router;
